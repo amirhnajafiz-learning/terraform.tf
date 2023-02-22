5c6503e9586a48f6afc69e3a31040053a7d43f2f
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-22 15:47:57 +0330

    update: git attributes

--- 01.Hello/app.ts
+++ 01.Hello/app.ts
@@ -1,6 +1,8 @@
 let message: string = 'Hello, World!';
+
 // create a new heading 1 element
 let heading = document.createElement('h1');
 heading.textContent = message;
+
 // add the heading the document
 document.body.appendChild(heading);
