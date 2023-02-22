06723d7cb7c2c944ccef50833d03aac6a11e7c50
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-22 15:55:58 +0330

    init: type

---
+++ 02.TypeAnnotations/app.js
@@ -0,0 +1,8 @@
+// create a type annotation variable
+var person;
+// instance should be the same as the type created
+// above.
+person = {
+    name: "Amir",
+    age: 22
+};
---
+++ 02.TypeAnnotations/app.ts
@@ -0,0 +1,12 @@
+// create a type annotation variable
+let person: {
+    name: string;
+    age: number
+};
+
+// instance should be the same as the type created
+// above.
+person = {
+    name: "Amir",
+    age: 22
+}
