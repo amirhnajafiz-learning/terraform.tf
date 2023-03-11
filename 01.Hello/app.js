7bbc32b989c473902367c368db1d52a5924c6e30
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-03-11 21:28:12 +0330

    Delete 01.Hello directory

--- 01.Hello/app.js
+++
@@ -1,6 +0,0 @@
-var message = 'Hello, World!';
-// create a new heading 1 element
-var heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
--- 01.Hello/app.ts
+++
@@ -1,8 +0,0 @@
-let message: string = 'Hello, World!';
-
-// create a new heading 1 element
-let heading = document.createElement('h1');
-heading.textContent = message;
-
-// add the heading the document
-document.body.appendChild(heading);
