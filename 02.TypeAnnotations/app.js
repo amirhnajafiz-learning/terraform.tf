b57f05d9fd6843dba4f5a11fab5b2565653a1176
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-03-11 21:28:46 +0330

    Delete 02.TypeAnnotations directory

--- 02.TypeAnnotations/app.js
+++
@@ -1,8 +0,0 @@
-// create a type annotation variable
-var person;
-// instance should be the same as the type created
-// above.
-person = {
-    name: "Amir",
-    age: 22
-};
--- 02.TypeAnnotations/functions.ts
+++
@@ -1,9 +0,0 @@
-// create a function type annotation
-let concatenate: (a: string, b: string) => string;
-
-// give value to function
-concatenate = function (a, b) {
-    return a + b;
-};
-
-console.log(concatenate("Amir", "hossein"))
